beb3425d86549f93f0661e292114617085ab2556
bkvikram007 <vikrambk9481@gmail.com>
2025-07-28 10:27:42 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      int i_val=10;
+      int j_val=20;
+      boolean chk =i_val<j_val;
+      System.out.println("chk value = " + chk);
+       
+      
+    }
+}
